935d954dbc26d1985a7f6cf0794a58fdd6984d2c
dsschapira <dsschapira@gmail.com>
2020-09-14 00:39:33 -0400

    add list objects and list buckets

--- zip.go
+++ zip.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -12,7 +13,7 @@ import (
 
 var (
 	svc        *s3.S3
-	s3Session  *session.Session
+	s3session  *session.Session
 	uploader   *s3manager.Uploader
 	downloader *s3manager.Downloader
 )
@@ -22,16 +23,41 @@ var accessKey = os.Getenv("S3_ACCESS_KEY")
 var secretKey = os.Getenv("S3_SECRET_KEY")
 
 func init() {
-	s3Session, _ = session.NewSession(&aws.Config{
+	s3session, err := session.NewSession(&aws.Config{
 		Region:      aws.String(region),
 		Credentials: credentials.NewStaticCredentials(accessKey, secretKey, ""),
 	})
+	if err != nil {
+		panic(err)
+	}
 
-	svc = s3.New(s3Session)
-	uploader = s3manager.NewUploader(s3Session)
-	downloader = s3manager.NewDownloader(s3Session)
+	svc = s3.New(s3session)
+	uploader = s3manager.NewUploader(s3session)
+	downloader = s3manager.NewDownloader(s3session)
+}
+
+func listBuckets() (resp *s3.ListBucketsOutput) {
+	resp, err := svc.ListBuckets(&s3.ListBucketsInput{})
+	if err != nil {
+		panic(err)
+	}
+
+	return resp
+}
+
+func listObjects(bucketname string) (resp *s3.ListObjectsV2Output) {
+	resp, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{
+		Bucket: aws.String(bucketname),
+	})
+
+	if err != nil {
+		panic(err)
+	}
+	return resp
 }
 
 func main() {
-
+	buckets := listBuckets()
+	objects := listObjects(*buckets.Buckets[0].Name)
+	fmt.Println(objects.Contents)
 }
